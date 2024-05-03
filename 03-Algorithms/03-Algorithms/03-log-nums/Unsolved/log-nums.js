d1ba80dc7b7b35c76f590da207f9b946349f4c5e
pannajota <jotapanna2014@gmail.com>
2024-05-03 14:59:05 +0630

    first commit

--- 03-Algorithms/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ 03-Algorithms/03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,7 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    for (1 = 0; < num; 1 = 1 + 1)
+        console.log()
+};
